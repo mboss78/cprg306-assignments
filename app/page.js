87bbe38751d34482653b3e9ca0c2697d8cc526e6
mboss78 <153876286+mboss78@users.noreply.github.com>
2024-05-27 15:15:19 -0600

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        <Link href="/week-2">Link: Week 2 Assignment</Link>
+      </p>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Mohamad Bark</p>
+      <Link href="https://github.com/mboss78/cprg306-assignments">
+      https://github.com/mboss78/cprg306-assignments
+      </Link>
+    </div>
+  );
+}
